cc825f1534ea6e7f4fa9b675925055151bc0bee9
Shuyoou <shuyou.zsy@alibaba-inc.com>
2020-07-30 19:14:35 +0800

    feat: update

---
+++ README.md
@@ -0,0 +1,1 @@
+# numToBinary
---
+++ src/num-to-binary.ts
@@ -0,0 +1,31 @@
+export function numToBinary(num: number): string {
+  if (typeof num !== 'number') {
+    throw new Error('num is not a number.');
+  }
+
+  let integer = parseInt(`${num}`);
+  let decimals = num - integer;
+
+  let remainder = 0;
+  let integerBinary = '';
+
+  while (integer !== 0) {
+    remainder = integer % 2;
+    integer = parseInt(`${integer / 2}`);
+    integerBinary = remainder + integerBinary;
+  }
+
+  let product = 0;
+  let decimalsBinary = '';
+
+  while (decimals !== 0) {
+    decimals = decimals * 2;
+    product = parseInt(`${decimals}`);
+    decimals -= product;
+    decimalsBinary += product;
+  }
+
+  return [integerBinary || '0', decimalsBinary].filter(item => !!item).join('.');
+}
+
+console.info(numToBinary(12.0));
